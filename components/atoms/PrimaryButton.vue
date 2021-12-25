8a4e9e3fcc6a05be8873ccda59249826ff62b195
larry <larry@larrynoMacBook-Pro.local>
2021-12-25 21:22:43 +0900

    feat: dev env fin

---
+++ components/atoms/PrimaryButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-button>I am a Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrimaryButton',
+}
+</script>
+
+<style lang="scss"></style>
