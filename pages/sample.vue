484acee6cdc3a1ce1f6f7feac184bbd0358e70c5
larry <larry@larrynoMacBook-Pro.local>
2021-12-26 23:55:28 +0900

    feat: primary input atom add

--- pages/sample.vue
+++ pages/sample.vue
@@ -1,12 +1,24 @@
 <template>
-  <AtomsPrimaryCard title="sample" description="sample card" />
+  <div>
+    <AtomsPrimaryInput
+      v-model="value"
+      title="sample"
+      label="please enter"
+      description="sample card"
+      required="true"
+    />
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   name: 'SamplePage',
-  data: () => ({}),
+  data: () => {
+    return {
+      value: '',
+    }
+  },
   methods: {},
 })
 </script>
